1a6faa779b37743a3d3ece1c7ec9cc0e3185701d
wangxb <wangxb@builds-iMac-3.local>
2018-04-28 10:00:42 +0800

    add config files

---
+++ billing_collect_new/collect_config.py
@@ -0,0 +1,58 @@
+import os
+
+if os.environ.get('BILLING_COLLECT_DEBUG'):
+    base_dir = '/users/wangxb/Work/key_files'
+else:
+    base_dir = '/oits/service/key_files'
+
+CPU_COLLECT_CONFIG = {
+    "GUANGZHOU": {
+        "IP": "182.92.197.51",
+        "PORT": 22,
+        "USER": "paratera_gz",
+        "PASSWORD": None,
+        "KEY_FILE": os.path.join(base_dir, "guangzhou/paratera_gz.id"),
+        "NODELIST": []
+    },
+    "GUANGZHOU_LON": {
+        # "IP": "10.171.66.215",
+        "IP": "182.92.197.51",
+        "PORT": 5577,
+        "USER": "paratera_gz",
+        "PASSWORD": None,
+        "KEY_FILE": os.path.join(base_dir, "guangzhou/paratera_gz.id"),
+        "NODELIST": []
+    },
+    "LVLIANG": {
+        "IP": "101.200.155.2",
+        "PORT": 2222,
+        "USER": "pp_slccc",
+        "PASSWORD": "Cloud@Papp&pAra",
+        "KEY_FILE": None,
+        "NODELIST": []
+    },
+    "PART1": {
+        "IP": "123.56.81.106",
+        "PORT": 2201,
+        "USER": "pp_cs",
+        "PASSWORD": "Cloud@Papp&pAra",
+        "KEY_FILE": None,
+        "NODELIST": []
+    },
+    "ERA": {
+        "IP": "explane.sccas.cn",
+        "PORT": 22,
+        "USER": "blsc",
+        "PASSWORD": None,
+        "KEY_FILE": None,
+        "NODELIST": []
+    },
+    "ParaGrid1": {
+        "IP": "58.213.64.36",
+        "PORT": 8803,
+        "USER": "paratera",
+        "PASSWORD": "N@nJ1n9.edu^HpC",
+        "KEY_FILE": None,
+        "NODELIST": []
+    }
+}
